34727e1e9ad39593d0148a8d63fe5cc68301066f
shinnosuke.yayama <yymsnsk@gmail.com>
2021-01-25 15:45:30 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,27 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  # フォロー処理
+  def follow(other_user)
+    # 自分自身はフォローしないよ
+    unless self == other_user
+      # すでに登録されていない場合は登録する。登録されていたらインスタンスを返却
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+  # アンフォロー処理
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    # relationshipが存在していたら消す
+    relationship.destroy if relationship
+  end
+  # フォロー確認
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
